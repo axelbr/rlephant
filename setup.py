d1abff4e78574104b70c1bfda2ad98fa172b88de
Axel <axel.brunnbauer@tuwien.ac.at>
2020-10-10 19:17:22 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='rlephant',
    version='1.0',
-   description='Simple tool to efficiently read and write episodes of MDP\'s to files.',
+   description='A simple tool to efficiently read and write episodes of MDP\'s to files.',
    author='Axel Brunnbauer',
    author_email='axel.brunnbauer@gmx.at',
    packages=['rlephant'],
