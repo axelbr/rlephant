24cd4b5982f150e4988c0e9289ae46c5ba825480
Axel <axel.brunnbauer@tuwien.ac.at>
2020-10-10 17:58:23 +0200

    added additional tests

--- elephant/entities.py
+++ elephant/entities.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Dict, Iterable
+from typing import Dict, Iterable, Union
 
 import numpy as np
 
@@ -11,6 +11,25 @@ class Transition:
     reward: float
     done: bool
 
+    def __eq__(self, other):
+        if not isinstance(other, Transition):
+            return False
+
+        if self.reward != other.reward or self.done != other.done:
+            return False
+
+        for k, v in self.observation.items():
+            comp = other.observation[k] == v
+            if not comp.all():
+                return False
+
+        for k, v in self.action.items():
+            comp = other.action[k] == v
+            if not comp.all():
+                return False
+
+        return True
+
 @dataclass
 class Episode:
 
@@ -19,27 +38,51 @@ class Episode:
     rewards: np.ndarray = field(default_factory=lambda: np.array([], dtype=np.float))
     done: np.ndarray = field(default_factory=lambda: np.array([], dtype=np.bool))
 
-    def __getitem__(self, item) -> Iterable[Transition]:
-        transitions = []
+    def _get_item(self, index: int) -> Transition:
+        obs = {}
+        for k, v in self.observations.items():
+            obs[k] = v[index, :]
+
+        action = {}
+        for k, v in self.actions.items():
+            action[k] = v[index, :]
+
+        reward = self.rewards[index]
+        done = self.done[index]
+        return Transition(observation=obs,
+                          action=action,
+                          reward=reward,
+                          done=done)
+
+    def _get_slice(self, item) -> Iterable[Transition]:
         start = item.start if item.start else 0
         stop = item.stop if item.stop else self.length
         step = item.step if item.step else 1
+
+        if start < 0:
+            start = self.length + start
+
+        if stop < 0:
+            stop = self.length + stop
+
+        if step < 0:
+            tmp = start
+            start = stop - 1
+            stop = tmp - 1
+
+        transitions = []
         for i in range(start, stop, step):
-            obs = {}
-            for k, v in self.observations.items():
-                obs[k] = v[i]
-
-            action = {}
-            for k, v in self.actions.items():
-                action[k] = v[i]
-
-            reward = self.rewards[i]
-            done = self.done[i]
-            transitions.append(Transition(observation=obs,
-                                          action=action,
-                                          reward=reward,
-                                          done=done))
+            t = self._get_item(i)
+            transitions.append(t)
         return transitions
+
+    def __getitem__(self, item) -> Union[Transition, Iterable[Transition]]:
+        if type(item) == int:
+            return self._get_item(item)
+        elif type(item) == slice:
+            return self._get_slice(item)
+        else:
+            raise IndexError
 
     def __iter__(self):
        return iter(self[:])
@@ -50,12 +93,14 @@ class Episode:
 
     def append(self, transition: Transition):
         for k, v in transition.observation.items():
+            v = np.expand_dims(v, axis=0)
             if k in self.observations:
                 self.observations[k] = np.vstack((self.observations[k], v))
             else:
                 self.observations[k] = v
 
         for k, v in transition.action.items():
+            v = np.expand_dims(v, axis=0)
             if k in self.actions:
                 self.actions[k] = np.vstack((self.actions[k], v))
             else:
--- tests/entities.py
+++ tests/entities.py
@@ -4,16 +4,26 @@ import numpy as np
 
 from elephant import Transition, Episode
 
+def make_transition(value: int = None):
+    if value is not None:
+        return Transition(observation={'obs_a': np.array([[value, value], [value,value]]), 'obs_b': np.array([value])},
+                   action={'action_a': np.array([value, -value]), 'action_b': np.array([value])},
+                   reward=value,
+                   done=True)
+    else:
+        return Transition(observation={'obs_a': np.random.uniform(-5, 5, size=(2,2)), 'obs_b': np.random.uniform(-5, 5, size=(2,))},
+                          action={'action_a': np.random.uniform(-5, 5, size=(2,)), 'action_b': np.random.uniform(-5, 5, size=(2,))},
+                          reward=np.random.uniform(-5, 5, size=(1,)),
+                          done=np.random.choice([True, False]))
+
 
 class TestEpisode(unittest.TestCase):
 
     def test_single_append(self):
-        transition = Transition(observation={'obs_a': np.array([1.0, 1.0]), 'obs_b': np.array([2.0])},
-                                action={'action_a': np.array([1.0, -1.0]), 'action_b': np.array([-3.0])},
-                                reward=1.0,
-                                done=True)
+        transition = make_transition()
         episode = Episode()
         episode.append(transition)
+
         self.assertEqual(episode.length, 1, 'Episode should have length 1 after appending a single transition.')
         self.assertEqual(episode.observations, transition.observation)
         self.assertEqual(episode.actions, transition.action)
@@ -21,14 +31,8 @@ class TestEpisode(unittest.TestCase):
         self.assertEqual(episode.done, transition.done)
 
     def test_multiple_appends(self):
-        transition1 = Transition(observation={'obs_a': np.array([1.0, 1.0]), 'obs_b': np.array([2.0])},
-                                action={'action_a': np.array([1.0, -1.0]), 'action_b': np.array([-3.0])},
-                                reward=1.0,
-                                done=True)
-        transition2 = Transition(observation={'obs_a': np.array([3.0, 4.0]), 'obs_b': np.array([2.0])},
-                                action={'action_a': np.array([7.0, -10.0]), 'action_b': np.array([-5.0])},
-                                reward=0.0,
-                                done=False)
+        transition1 = make_transition(value=1)
+        transition2 = make_transition(value=2)
         episode = Episode()
         episode.append(transition1)
         episode.append(transition2)
@@ -39,14 +43,34 @@ class TestEpisode(unittest.TestCase):
         self.assertEqual(episode.rewards.shape, (2,))
         self.assertEqual(episode.done.shape, (2,))
 
-        def test_iterator():
-            transition1 = Transition(observation={'obs_a': np.array([1.0, 1.0]), 'obs_b': np.array([2.0])},
-                                     action={'action_a': np.array([1.0, -1.0]), 'action_b': np.array([-3.0])},
-                                     reward=1.0,
-                                     done=True)
+    def test_iterator(self):
+        episode = Episode()
+        transitions = []
+        for i in range(10):
+            t = make_transition(i)
+            episode.append(t)
+            transitions.append(t)
+
+        for i, t in enumerate(episode):
+            self.assertEqual(t, transitions[i])
 
 
+    def test_slicing(self):
+        episode = Episode()
+        transitions = []
+        for i in range(10):
+            t = make_transition(i)
+            episode.append(t)
+            transitions.append(t)
 
+        self.assertEqual(transitions[1], episode[1])
+        self.assertEqual(transitions[:3], episode[:3])
+        self.assertEqual(transitions[6:], episode[6:])
+        self.assertEqual(transitions[3:6], episode[3:6])
+        self.assertEqual(transitions[-5:-3], episode[-5:-3])
+        self.assertEqual(transitions[::-1], episode[::-1])
+        self.assertEqual(transitions[:-2], episode[:-2])
+        self.assertEqual(transitions[-1:-2], episode[-1:-2])
 
 
 
