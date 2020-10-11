fa126cdd54ca8ca1b4bcf35849317db9482863e5
Axel <axel.brunnbauer@tuwien.ac.at>
2020-10-11 10:28:18 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
-   name='rlephant',
-   version='1.0',
-   description='A simple tool to efficiently read and write episodes of MDP\'s to files.',
-   author='Axel Brunnbauer',
-   author_email='axel.brunnbauer@gmx.at',
-   packages=['rlephant'],
-   install_requires=['numpy', 'h5py']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rlephant',
+    version='1.0.0',
+    description='A simple tool to efficiently read and write episodes of MDP\'s to files.',
+    author='Axel Brunnbauer',
+    author_email='axel.brunnbauer@gmx.at',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/axelbr/rlephant",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy', 'h5py']
 )
